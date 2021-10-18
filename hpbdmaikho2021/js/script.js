f4e040570b54400391549c73bc65aa594cfc7908
mduc.cntt <mduc.cntt@gmail.com>
2021-10-18 11:45:37 +0700

    UpdateUpdate HBBD mai kho 2021

--- hpbdmaikho2021/js/script.js
+++ hpbdmaikho2021/js/script.js
@@ -1,7 +1,7 @@
 /* https://www.sitepoint.com/build-javascript-countdown-timer-no-dependencies/ */
 
 function getTimeRemaining(endtime){
-  var t = Date.parse(endtime) - Date.parse(new Date());
+  var t = Date.parse(new Date(endtime)) - Date.parse(new Date());
   var seconds = Math.floor( (t/1000) % 60);
   var minutes = Math.floor( (t/1000 / 60) % 60);
   var hours = Math.floor( (t/(1000 * 60 * 60)) % 24);
